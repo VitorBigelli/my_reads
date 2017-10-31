ee4274434cc3b4e95b53529fb8fa9e9b4cbb01dc
Vitor <vitorbigelli@hotmail.com>
2017-10-31 04:25:09 -0200

    feat: Import BooksAPI and create bookcases sections

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import logo from './logo.svg';
+import * as BooksAPI from './utils/BooksAPI'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-        <Route exact path='/' render={ () => (
-        	<h2> My reads </h2>
-        )}/>
 
-        <Route exact path='/search' render={ () => (
-        	<h2> Book search </h2>
-        )}/>
+	state = {
+		currentlyReading: [],
+		wantToRead: [],
+		read: [],
+		books: []
+	}
+
+	componentDidMount() {
+		BooksAPI.getAll().then( (books) => (
+			this.setState( {books} )
+		))
+	}
+
+	render() {
+	return (
+	  <div className="App">
+	    
+	    <Route exact path='/' render={ () => (
+	    	<div className='bookcases'>
+	    		<h2 className='my-reads-header'> My reads </h2>
+
+	    		<div className='currently-reading-list'>
+	    			<h3> Currently reading </h3>
+
+	    		</div>
+
+	    		<div className='want-to-read-list'>
+	    			<h3> Want to read </h3>
+
+	    		</div>
+
+	    		<div className='read-list'>
+	    			<h3> Read </h3>
+
+	    		</div>
+	    	</div>
+	    )}/>
+
+	    <Route exact path='/search' render={ () => (
+	    	<h2> Book search </h2>
+	    )}/>
 
 
-      </div>
-    );
-  }
+	  </div>
+	);
+	}
 }
 
 export default App;
