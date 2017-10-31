9d0cd9e9a105be73f0d7af61eec1db503a757be7
Vitor <vitorbigelli@hotmail.com>
2017-10-31 04:07:48 -0200

    feat: Define app routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +7,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <Route exact path='/' render={ () => (
+        	<h2> My reads </h2>
+        )}/>
+
+        <Route exact path='/search' render={ () => (
+        	<h2> Book search </h2>
+        )}/>
+
+
       </div>
     );
   }
