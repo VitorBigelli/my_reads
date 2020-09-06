6446ba9850f1f24c1be52f9cfbd91e3b248af4ea
Vitor <bigellivitor@gmail.com>
2020-09-06 16:47:43 -0300

    Allow user move and reorder books

--- src/App.js
+++ src/App.js
@@ -16,17 +16,18 @@ import {
 
 import {ReactTinyLink} from 'react-tiny-link'
 
-import reading_image_1 from './assets/reading_1.jpeg'
-import reading_image_2 from './assets/reading_2.jpeg'
-import reading_image_3 from './assets/reading_3.jpeg'
+import reading_image_1 from './assets/reading_image_2.png'
+import reading_image_2 from './assets/reading_image_1.png'
 import { Container, Card, Row, Col } from 'react-bootstrap';
+
+import { move, reorder } from './utils/helpers'
 
 
 class App extends Component {
 
 	constructor(props) {
 		super(props); 
-		this.state = { books: books, searchResult: [], loading: false }
+		this.state = { ...books, searchResult: [], loading: false }
 	}
 
 	// This function updates the 'myReads' in localStorage
@@ -110,12 +111,9 @@ class App extends Component {
 
 	// This function is called when the user change 
 	// a book to a new bookshelf
-	onChangeBookshelf = (event, book) => {
-		event.preventDefault() 
+	onChangeBookshelf = (book) => {
 		
 		let newBooks = this.state.books.filter( _ => _.id !== book.id)
-
-		book.shelf = event.target.value
 
 		if (book.shelf === 'none') {
 			this.deleteBook(book.id)
@@ -127,32 +125,55 @@ class App extends Component {
 		this.updateLocalStorage(newBooks)
 	}
 
-	search = (query) => {
-		this.setState({
-			loading: true
-		})
-
-		BooksAPI.search(query, 20).then( (books) => {
-			
-			let result = []
-
-			if (Object.prototype.toString.call( books ) === '[object Array]') {
-				result = books.filter( book => {
-					this.state.books.map( _ => {
-						if (_.id === book.id) {
-							book.shelf = _.shelf
-						}
-					})
-					return true;
-				})
-			} 
-
-			this.setState({
-				searchResult: result,
-				loading: false 
-			})
-		})
+	getList(status) {
+		return this.state.books[status]
 	}
+
+	onDragEnd(result) { 
+		const { source, destination } = result 
+
+		console.log(typeof move)
+
+		if (!destination.droppableId) {
+			return null 
+		} else {
+
+			if (destination.droppableId === source.droppableId) {
+
+				const items = reorder(
+					this.state[source.droppableId],
+					source.index, 
+					destination.index
+				) 
+
+
+				this.setState({
+					[source.droppableId]: items
+				})
+
+			} else {
+				
+				const result = move(
+					this.state[source.droppableId],
+					this.state[destination.droppableId],
+					source,
+					destination
+				)
+
+				this.setState({
+					[source.droppableId]: result[source.droppableId], 
+					[destination.droppableId]: result[destination.droppableId]
+				})
+
+			}
+
+		}
+
+
+		
+
+	}
+
 
 	// Clear the localStorage
 	restoreDefault = () => {
@@ -164,39 +185,34 @@ class App extends Component {
 		const { searchResult, loading } = this.state
 
 		const bookshelfs = [
-			{ title: 'Want to Read', shelf: 'listed' }, 
+			{ title: 'Want to Read', shelf: 'want_to_read' }, 
 			{ title: 'Currently Reading', shelf: 'reading' }, 
 			{ title: 'Read', shelf: 'read' }
 		]
-
-		console.log(books)
 
 		return (
 		  <div className="App">
 			<img src={reading_image_1} alt='...' className='reading_image_1' />
 			<img src={reading_image_2} alt='...' className='reading_image_2' />
-			<img src={reading_image_3} alt='...' className='reading_image_3' />
 		    
 		    <Route exact path='/' render={ () => (
 		    	<Container className='bookshelfs'>
-	
-		    		<Link to='/search' className='books-search-link' />
-		    		<Link to='/manage' className='books-manage' />
+			    		<Link to='/manage' className='books-manage' />
 
 		    		<h2 className='my-reads-header'> Vitor Bigelli's reads </h2>
 
 					<div className='d-flex flex-row align-items-start justify-content-start'>
-						<DragDropContext>
+						<DragDropContext onDragEnd={ (e) => this.onDragEnd(e)}>
 							{ bookshelfs.map( (bookshelf, index) => (
-								<Droppable droppableId={`droppable droppable_${index}`} key={index} > 
+								<Droppable droppableId={bookshelf.shelf} key={index} > 
 									{(provided, snapshot) => (
 										<div	
 											ref={provided.innerRef}
 											className={`droppable droppable_${index}`}
 											>
 											<h5> { bookshelf.title } </h5>
-											{ books.map((item, index) => (
-												item.status === bookshelf.shelf && <Draggable
+											{ this.state[bookshelf.shelf].map((item, index) => (
+												<Draggable
 													key={item.id}
 													draggableId={item.id}
 													index={index}>
@@ -228,14 +244,6 @@ class App extends Component {
 		    	</Container>
 		    )}/>
 
-		    <Route exact path='/search' render={ () => (
-   				<BooksSearch 
-   					onChangeBookshelf={ (event, book) => this.onChangeBookshelf(event, book) }
-   					search={(query) => this.search(query)}
-   					books={searchResult}
-   					loading={loading}
-   				/>
-		    )}/>
 
 		    <Route exact path='/register' render={ ({history}) => (
 		    	<RegisterBook onSubmit={ values => {
--- src/data/books.js
+++ src/data/books.js
@@ -1,27 +1,34 @@
-export const books = [
-    {
-        "id": "0",
-        "amazon_link": "https://www.amazon.com.br/Ride-Lifetime-Lessons-Learned-Company-ebook/dp/B07PF6XTD8",
-        "status": "reading"
-    },
-    {
-        "id": "1",
-        "amazon_link": "https://www.amazon.com/Drunkards-Walk-Randomness-Rules-Lives/dp/0307275175",
-        "status": "read"
-    },
-    {
-        "id": "2",
-        "amazon_link": "https://www.amazon.com/Os-Inovadores-Biografia-Revolucao-Portugues/dp/8535925023",
-        "status": "read"
-    },
-    {
-        "id": "3",
-        "amazon_link": "https://www.amazon.com.br/How-Create-Mind-Thought-Revealed/dp/0143124048",
-        "status": "read"
-    },
-    {
-        "id": "4",
-        "amazon_link": "https://www.amazon.com/Crime-Castigo-Em-Portuguese-Brasil/dp/8572329528",
-        "status": "reading"
-    }
-]
+export const books = {
+    "read": [
+        {
+            "id": "1",
+            "amazon_link": "https://www.amazon.com/Drunkards-Walk-Randomness-Rules-Lives/dp/0307275175"
+        },
+        {
+            "id": "2",
+            "amazon_link": "https://www.amazon.com/Os-Inovadores-Biografia-Revolucao-Portugues/dp/8535925023"
+        },
+        {
+            "id": "3",
+            "amazon_link": "https://www.amazon.com.br/How-Create-Mind-Thought-Revealed/dp/0143124048"
+        }
+    ], 
+    "reading": [
+        {
+            "id": "0",
+            "amazon_link": "https://www.amazon.com.br/Ride-Lifetime-Lessons-Learned-Company-ebook/dp/B07PF6XTD8"
+        },
+        {
+            "id": "4",
+            "amazon_link": "https://www.amazon.com/Crime-Castigo-Em-Portuguese-Brasil/dp/8572329528"
+        }
+    ],
+    "want_to_read": [
+        {
+            "id": "5", 
+            "amazon_link": "https://www.amazon.com/Lessons-21st-Century-Yuval-Harari/dp/0525512179"
+        }
+    ]
+
+}
+    
